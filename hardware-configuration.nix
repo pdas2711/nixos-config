a8ca42026f5fa2f7415f3faa758d29c6467d6980
Pial Das <pdas2711@proton.me>
2025-01-14 21:45:32 -0500

    Added a static IP to one of the network interfaces.

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -45,10 +45,13 @@
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp6s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
+	networking.useDHCP = lib.mkDefault true;
+	# networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
+	networking.interfaces.enp6s0.ipv4.addresses = [ {
+		address = "169.168.1.1";
+		prefixLength = 30;
+	} ];
+	# networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
