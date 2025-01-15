cdd0856f1e63dc0afd6e0d17b7f02bf865a491f8
Pial Das <pdas2711@proton.me>
2025-01-15 03:40:43 -0500

    Only allow public key-based authentication and disable root login for SSH.

--- configuration.nix
+++ configuration.nix
@@ -142,7 +142,13 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+	services.openssh = {
+		enable = true;
+		settings = {
+			PasswordAuthentication = false;
+			PermitRootLogin = "no";
+		};
+	};
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
