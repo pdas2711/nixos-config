d4fe5ee26e2311f46b4d36bb1dff6232d56ad3dd
Pial Das <pdas2711@proton.me>
2025-01-16 01:12:39 -0500

    Changed initrd's SSH port from 23 to 2222.

--- configuration.nix
+++ configuration.nix
@@ -28,7 +28,7 @@
 			ssh = {
 				shell = "/bin/cryptsetup-askpass";
 				enable = true;
-				port = 23;
+				port = 2222;
 				authorizedKeys = [
 					"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHMTMLADSEtAhxuBzgzngIhx7LZfo5vfnTaPzyRhPBx root@xansaware"
 				];
