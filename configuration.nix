6b0df8c04db8df39f7f96e1db2c3f706ffe1dcb8
Pial Das <pdas2711@proton.me>
2025-01-16 14:18:46 -0500

    Enabled iwd in configuration.

--- configuration.nix
+++ configuration.nix
@@ -37,10 +37,11 @@
 		};
 	};
 
-  networking.hostName = "xansaware"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+	networking = {
+		hostName = "xansaware"; # Define your hostname.
+		wireless.iwd.enable = true;  # Enables wireless support via iwd.
+		networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+	};
 
   # Set your time zone.
   time.timeZone = "America/New_York";
