4113b23594af0d50318a13ba603a8350ba6018e8
Pial Das <pdas2711@proton.me>
2025-01-23 15:18:51 -0500

    Fixed wake script by using broadcast ip instead of host ip to send magic packet.

--- xansawarejb/wake.nix
+++ xansawarejb/wake.nix
@@ -10,8 +10,9 @@ port=$(${pkgs.jq}/bin/jq .port /etc/wake_config.json)
 initrd_hostname=$(${pkgs.jq}/bin/jq .initrd_hostname /etc/wake_config.json | sed 's/"//g')
 initrd_port=$(${pkgs.jq}/bin/jq .initrd_port /etc/wake_config.json)
 mac=$(${pkgs.jq}/bin/jq .mac /etc/wake_config.json | sed 's/"//g')
+broadcast_ip=$(${pkgs.jq}/bin/jq .broadcast_ip /etc/wake_config.json | sed 's/"//g')
 
-${pkgs.wol}/bin/wol --ipaddr ''${hostname} ''${mac}
+${pkgs.wol}/bin/wol --ipaddr ''${broadcast_ip} ''${mac}
 
 echo "Waiting for the machine to fully boot..."
 while true; do
@@ -20,7 +21,7 @@ while true; do
                 exit
         elif [[ $(${pkgs.nmap}/bin/nmap -p ''${initrd_port} ''${initrd_hostname} | grep "''${initrd_port}/tcp" | awk '{print $2}') == "open" ]]; then
                 echo "The OS is partially booted. Unlocking..."
-                cat /srv/secrets/luks/luks.key | ${pkgs.openssh}/bin/ssh -p ''${initrd_port} root@''${initrd_hostname}
+                cat /srv/secrets/luks/luks.key | ${pkgs.openssh}/bin/ssh -p ''${initrd_port} -o IdentitiesOnly=yes -i ~/.ssh/init root@''${initrd_hostname}
         else
                 sleep 5s
         fi
