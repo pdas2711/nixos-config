9d2cead14a0a1c7afdd11541c841598da2204ff0
Pial Das <pdas2711@proton.me>
2025-09-14 01:00:37 -0400

    Set removable for grub since motherboard firmware requires default naming scheme for efi files.

--- xansaserver/configuration.nix
+++ xansaserver/configuration.nix
@@ -13,11 +13,11 @@
 	# Boot Configuration
 	boot = {
 		loader = {
-			efi.canTouchEfiVariables = true;
 			# Use Grub for the bootloader.
 			grub = {
 				enable = true;
 				efiSupport = true;
+				efiInstallAsRemovable = true;
 				device = "nodev";
 			};
 		};
