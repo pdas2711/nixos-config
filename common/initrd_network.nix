20c3b5b8d18c8f8b62692117cd7b1093f20e80b9
Pial Das <pdas2711@proton.me>
2025-12-17 22:11:11 -0500

    Added networking configuration for the initrd environment. This applies to systems with their root filesystem encrypted with LUKS that want remote unlocking.

---
+++ common/initrd_network.nix
@@ -0,0 +1,19 @@
+{ ... }: {
+	# Enable networking in initrd when unlocking LUKS root
+	boot.initrd = {
+		systemd.users.root.shell = "/bin/cryptsetup-askpass";
+		network = {
+			enable = true;
+			ssh = {
+				shell = "/bin/cryptsetup-askpass";
+				enable = true;
+				port = 2222;
+				authorizedKeys = [
+					"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHMTMLADSEtAhxuBzgzngIhx7LZfo5vfnTaPzyRhPBx root@xansaware"
+					"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPbTg0cEmjqNJd9rh2eEL4v4WVdIhsWTq19glZ8AnspK root@xansawarejb"
+				];
+				hostKeys = [ "/etc/ssh/initrd/ssh_host_ed25519_key" ];
+			};
+		};
+	};
+}
