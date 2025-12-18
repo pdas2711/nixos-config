aa36f417fc0e0d4cdb4f642cbf1e3f7d9cd4178e
Pial Das <pdas2711@proton.me>
2025-12-17 22:07:27 -0500

    Added common applications that all systems will use.

---
+++ common/applications.nix
@@ -0,0 +1,67 @@
+# Common applications
+
+{ pkgs, pkgsUnstable, ... }: {
+	environment.systemPackages = with pkgs; [
+		fastfetch
+		tmux
+		wget
+		pkgsUnstable.git-filter-repo
+		gnupg
+		pinentry-tty
+		vlock
+		gopass
+		zoxide
+		zsh-syntax-highlighting
+		python3
+		pipx
+		ranger
+		stow
+		trash-cli
+		pciutils
+		openssl
+		qrencode
+		oathToolkit
+		mpv
+		rtorrent
+		jq
+		parted
+		zip
+		unzip
+		logrotate
+		pv
+		pwgen
+		file
+		ncdu
+		htop
+		libcaca
+		libsixel
+		cargo
+		rustc
+		gcc
+		nbd
+		elinks
+		cowsay
+		newsboat
+	];
+
+	# Zsh
+	programs.zsh = {
+		enable = true;
+		syntaxHighlighting.enable = true;
+	};
+	
+	# UWSM
+	programs.uwsm.enable = true;
+
+	# Git Config
+	programs.git = {
+		enable = true;
+		config.init.defaultBranch = "main";
+	};
+	
+	# Neovim
+	programs.neovim = {
+		enable = true;
+		defaultEditor = true;
+	};
+}
