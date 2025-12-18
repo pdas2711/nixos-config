c99e32a9fc1b7fc814a3f17ad17020abd5efc6c6
Pial Das <pdas2711@proton.me>
2025-12-18 18:27:08 -0500

    Added host-specific hardware-related configuration for host xansago.

---
+++ hosts/xansago/hardware.nix
@@ -0,0 +1,15 @@
+{ modulesPath, ... }: {
+	imports = [
+		(modulesPath + "/installer/scan/not-detected.nix")
+	];
+
+	# Kernel Modules
+	boot.initrd.availableKernelModules = [ "nvme" "usb_storage" "usbhid" ];
+	boot.kernelModules = [ "kvm-intel" ];
+
+	# Nixpkg platform target
+	nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+	
+	# CPU microcode
+	hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
