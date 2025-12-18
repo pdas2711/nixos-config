540b18a43326d47f059accd98a570e01f965a039
Pial Das <pdas2711@proton.me>
2025-12-17 22:11:45 -0500

    Basic configuration for NetworkManager.

---
+++ common/networking.nix
@@ -0,0 +1,16 @@
+{ ... }: {
+	# NetworkManager
+	networking.networkmanager.enable = true;
+
+	# Enable routing
+	boot.kernel.sysctl = {
+		"net.ipv4.conf.all.forwarding" = lib.mkDefault true;
+	        "net.ipv4.conf.default.forwarding" = lib.mkDefault true;
+	};
+	
+	# Enable NAT
+	networking.nat.enable = true;
+
+	# Enable NFTables
+	networking.nftables.enable = true;
+}
