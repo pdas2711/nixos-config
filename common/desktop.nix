20a1528cc81178d036ab9c7b6ab55e4209e01a35
Pial Das <pdas2711@proton.me>
2025-12-17 22:08:37 -0500

    Added desktop configuration for systems that will be use a compositor, mainly hyprland. Also added enabling flatpak since flatpak apps are GUI apps.

---
+++ common/desktop.nix
@@ -0,0 +1,35 @@
+{ pkgs, ... }: {
+	# Hyprland
+	programs.hyprland = {
+		enable = true;
+		xwayland.enable = true;
+		withUWSM = true;
+	};
+	
+	# UWSM
+	programs.uwsm.waylandCompositors = {
+		hyprland = {
+			prettyName = "Hyprland";
+			binPath = "${pkgs.hyprland}/bin/Hyprland";
+		};
+	};
+	
+	# GUI Applications
+	environment.systemPackages = with pkgs; [
+		alacritty
+		zathura
+		libreoffice
+		bemenu
+		brave
+		hyprpaper
+		hyprlock
+		hyprshot
+		waybar
+		wl-clipboard
+		wlsunset
+		pavucontrol
+	];
+	
+	# Enable Flatpak
+	services.flatpak.enable = true;
+}
