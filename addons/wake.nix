bd8ae205035f5da9bed2a59cdfe0301801911aff
Pial Das <pdas2711@proton.me>
2025-12-18 22:49:14 -0500

    Created wrapper nix function to add wake shell script.

---
+++ addons/wake.nix
@@ -0,0 +1,18 @@
+{ ... }: {
+	# Group creation for wake
+	users.groups.wake = {};
+
+	# Sudoers rule to run wake script without password for wake group
+	security.sudo.extraRules = [
+		{
+			groups = [ "wake" ];
+			commands = [ {
+				command = "/run/current-system/sw/bin/wake";
+				options = [ "NOPASSWD" ];
+			} ];
+		}
+	];
+
+	# Add wake to environment packages
+	environment.systemPackages = [ (import ./wake.nix { inherit pkgs; }) ];
+}
