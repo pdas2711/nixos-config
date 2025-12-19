b785622e00ca6d50115089316f84de2d2147279d
Pial Das <pdas2711@proton.me>
2025-12-18 20:49:52 -0500

    Added UUIDs for host xansago.

--- hosts/xansago/filesystems.nix
+++ hosts/xansago/filesystems.nix
@@ -6,18 +6,18 @@
 		imports = [ common_dir + ./default_mounts.nix ];
 
 	# LUKS root device target
-	boot.initrd.luks.devices."root".device = "";
+	boot.initrd.luks.devices."root".device = "adde90b1-12c2-4d2d-ba77-c36bb83ce50f";
 
 	# Filesystem for boot device
 	filesystems."/boot" = {
-		device = "";
+		device = "A85E-E5AF";
 		fsType = "vfat";
 		options = [ "fmask=0022" "dmask=0022" ];
 	};
 
 	# Device target for root filesystem
 	let
-		root_target = "/dev/disk/by-uuid/UUID";
+		root_target = "/dev/disk/by-uuid/52cf6217-82b9-43e2-bbc2-81b69c466c97";
 	in {
 		fileSystems = {
 			"/".device = root_target;
