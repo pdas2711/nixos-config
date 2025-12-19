862c5a5c617b70fc3cb9929290b0f0479bbe9e5a
Pial Das <pdas2711@proton.me>
2025-12-18 22:48:52 -0500

    Created wrapper nix function to add power-timer shell script.

---
+++ addons/power_timer.nix
@@ -0,0 +1,18 @@
+{ ... }: {
+	# Group creation for power-timer
+	users.groups.power-timer = {};
+
+	# Sudoers rule to run without a password for power-timer group
+	security.sudo.extraRules = [
+		{
+			groups = [ "power-timer" ];
+			commands = [ {
+				command = "/run/current-system/sw/bin/power-timer";
+				options = [ "NOPASSWD" ];
+			} ];
+		}
+	];
+
+	# Add power-timer to environment packages
+	environment.systemPackages = [ (import ./power_timer_sh.nix { inherit pkgs; }) ];
+}
