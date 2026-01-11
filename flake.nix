1c85a2401e5b6312ea90f9c957ae9f5e0c528290
Pial Das <pdas2711@proton.me>
2026-01-11 16:33:31 -0500

    Updated NixOS to 25.11.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 	description = "NixOS Configuration";
 
 	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 		nixpkgsUnstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 		nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 	};
