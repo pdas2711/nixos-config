3e800e883d70cc6596a484cf418ac3e3c84a1e11
Pial Das <pdas2711@proton.me>
2026-02-11 20:30:50 -0500

    Firewall rule for wireguard is moved to wireguard_init.nix and is imported.

--- common/wireguard_server.nix
+++ common/wireguard_server.nix
@@ -1,7 +1,9 @@
 { ... }: {
+	# Import firewall routing rule for wireguard
+	import = [ ./wireguard.init ]
+
 	# Wireguard Server Setup
 	networking.wireguard.enable = true;
-	networking.nat.internalInterfaces = [ "wg0" ];
 	networking.wg-quick.interfaces.wg0.configFile = "/etc/wireguard/wg0.conf";
 
 	# Open Firewall
