06171b2611318f526eeeeec2ddd9c2d3d572a4bb
Pial Das <pdas2711@proton.me>
2026-03-08 21:12:55 -0400

    Indentation typo.

--- hosts/xansaserver/hardware.nix
+++ hosts/xansaserver/hardware.nix
@@ -4,7 +4,7 @@
 	];
 
 	# Kernel Modules
-	  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_usb_sdmmc" ];
+	boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_usb_sdmmc" ];
 	boot.kernelModules = [ "kvm-intel" ];
 
 	# Nixpkg platform target
