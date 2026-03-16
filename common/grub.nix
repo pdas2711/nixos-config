35fa9872890fc9795b81433a32bb9111d14780cd
Pial Das <pdas2711@proton.me>
2026-03-15 22:33:44 -0400

    Fixed variable 'boot' being undefined.

--- common/grub.nix
+++ common/grub.nix
@@ -1,8 +1,8 @@
-{ lib, ... }: {
+{ lib, config, ... }: {
 	boot = {
 		loader = {
 			# This is relevant if the UEFI on the host is doesn't recognize custom EFI names
-			efi.canTouchEfiVariables = if (boot.loader.grub.efiInstallAsRemovable == false) then true else false;
+			efi.canTouchEfiVariables = if (config.boot.loader.grub.efiInstallAsRemovable == false) then true else false;
 			# Use Grub for the bootloader.
 			grub = {
 				enable = true;
