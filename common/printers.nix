a98707495945709e6658d9d65ca1c7950f60b842
Pial Das <pdas2711@proton.me>
2026-04-25 17:24:24 -0400

    Added missing option to enable sane.

--- common/printers.nix
+++ common/printers.nix
@@ -14,9 +14,12 @@
 	};
 
 	# Enable Airscan support for scanner backend
-	hardware.sane.extraBackends = [
-		pkgs.sane-airscan
-		pkgs.hplipWithPlugin  # HP Officejet 5740 e-AIO uses proprietary HP plugin. This printer also supports Airscan for scanning.
-	];
+	hardware.sane = {
+		enable = true;
+		extraBackends = [
+			pkgs.sane-airscan
+			pkgs.hplipWithPlugin  # HP Officejet 5740 e-AIO uses proprietary HP plugin. This printer also supports Airscan for scanning.
+		];
+	};
 	services.udev.packages = [ pkgs.sane-airscan ];
 }
