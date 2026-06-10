c2fc08d579790ee56848307aa26ae0f3b3210e15
Pial Das <pdas2711@proton.me>
2026-06-10 18:40:56 -0400

    Fixed command specification for authorized keys.

--- common/initrd_network.nix
+++ common/initrd_network.nix
@@ -7,8 +7,8 @@
 				enable = true;
 				port = 2222;
 				authorizedKeys = [
-					"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHMTMLADSEtAhxuBzgzngIhx7LZfo5vfnTaPzyRhPBx root@xansaware command=\"systemctl default\""
-					"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPbTg0cEmjqNJd9rh2eEL4v4WVdIhsWTq19glZ8AnspK root@xansawarejb command=\"systemctl default\""
+					"command=\"systemctl default\" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHMTMLADSEtAhxuBzgzngIhx7LZfo5vfnTaPzyRhPBx root@xansaware"
+					"command=\"systemctl default\" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPbTg0cEmjqNJd9rh2eEL4v4WVdIhsWTq19glZ8AnspK root@xansawarejb"
 				];
 				hostKeys = [ "/etc/ssh/initrd/ssh_host_ed25519_key" ];
 			};
